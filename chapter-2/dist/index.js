03398a0a796340e66b16105e490fb875dce10d93
Josh Newham <joshnewham@live.com>
2021-05-16 08:28:01 +0100

    Completed Chapter 2

---
+++ chapter-2/dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+let a = 1 + 2;
+let b = a + 3;
+let c = {
+    apple: a,
+    banana: b,
+};
+let d = c.apple * 4;
+let arr = [
+    ["Hello", 1, true],
+    [1, "Hello"],
+];
+console.log(a, b, c, d, arr);
+//# sourceMappingURL=index.js.map
---
+++ chapter-2/src/index.ts
@@ -0,0 +1,12 @@
+let a = 1 + 2;
+let b = a + 3;
+let c = {
+  apple: a,
+  banana: b,
+};
+let d = c.apple * 4;
+let arr = [
+  ["Hello", 1, true],
+  [1, "Hello"],
+];
+console.log(a, b, c, d, arr);

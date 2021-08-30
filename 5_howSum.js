cdd66939b5fab743e3c0fad5b79dc1123c3d3aa6
Josh Newham <joshnewham@live.com>
2021-08-30 15:05:41 +0100

    Implemented a simple howSum algorithm, heavily based on canSum

--- 5_howSum.js
+++ 5_howSum.js
@@ -17,4 +17,26 @@
 
 // Base cases:
 // targetSum = 0 -> []
-// targetSum != 0 -> null
+// targetSum < 0 -> null
+
+const howSum = (targetSum, numbers) => {
+  if (targetSum === 0) return [];
+  if (targetSum < 0) return null;
+
+  for (let num of numbers) {
+    const remainder = targetSum - num;
+    const remainderResult = howSum(remainder, numbers);
+    if (remainderResult !== null) {
+      // If there is at least one way to generate sum, do an early return
+      return [...remainderResult, num];
+    }
+  }
+
+  return null;
+};
+
+console.log(howSum(7, [2, 3]));
+console.log(howSum(7, [5, 3, 4, 7]));
+console.log(howSum(7, [2, 4]));
+console.log(howSum(8, [2, 3, 5]));
+console.log(howSum(300, [7, 14])); // very slow!

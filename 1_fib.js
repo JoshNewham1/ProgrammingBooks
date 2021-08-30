b725196ebe5fafe8163ab85917a38818482bc5d4
Josh Newham <joshnewham@live.com>
2021-08-30 13:01:41 +0100

    Annotated classical fib implementation

--- 1_fib.js
+++ 1_fib.js
@@ -1,3 +1,10 @@
+// Classic recursive fib - O(2^n) time as results in a binary tree of two function calls per pass
+//        fib(7)             1 node
+//    f(6)      f(5)         2 nodes
+// f(5) f(4)  f(4) f(3)      4 nodes
+// ...                       2^n nodes
+// O(n) space as stack frames are popped when base case of one branch is reached
+// Note that some of these function calls are reused further down the tree (e.g. fib(5))
 const fib = (n) => {
   if (n <= 2) {
     return 1;
@@ -8,4 +15,4 @@ const fib = (n) => {
 console.log(fib(6));
 console.log(fib(7));
 console.log(fib(8));
-console.log(fib(50)); // very slow!
+console.log(fib(50)); // very slow! (about a quadrillion steps)

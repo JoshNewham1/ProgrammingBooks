5febda9c06af3eb0344c43325610f440c344d57d
Josh Newham <joshnewham@live.com>
2023-05-14 19:59:33 +0100

    Annotated Hello World

--- 01_HelloWorld/src/main.cpp
+++ 01_HelloWorld/src/main.cpp
@@ -1,7 +1,18 @@
+// Preprocessor statements (begin with #)
+// Processed just before compilation. In this case, just include all the contents of the iostream header file in this file
+// Header files aren't compiled, they are just included in .cpp files that are compiled
 #include <iostream>
 
+void Log(const char* message) 
+{
+    std::cout << message << std::endl;
+}
+
+// Main function (entry point)
+// Special case that doesn't have to actually return anything (defaults to returning 0)
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
-    return 0;
+    // The << operator is a function that pushes "Hello World!" and cout INTO the cout function
+    Log("Hello World!");
+    std::cin.get();
 }

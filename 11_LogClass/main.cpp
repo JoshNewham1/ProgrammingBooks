b65470c9c2ebb9073fd0be74eb6f08024bd55893
Josh Newham <joshnewham@live.com>
2023-05-21 16:03:22 +0100

    Created a basic log class

---
+++ 11_LogClass/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+class Log
+{
+    
+// Variables
+public:
+    const int LogLevelError = 0;
+    const int LogLevelWarning = 1;
+    const int LogLevelInfo = 2;
+private:
+    int m_LogLevel = LogLevelInfo; // m_ convention tells us this is a class member variable not a local
+
+// Methods
+public:
+    void SetLevel(int level)
+    {
+        m_LogLevel = level;
+    }
+
+    void Error(const char* message)
+    {
+        if (m_LogLevel >= LogLevelError)
+            std::cout << "[ERROR]: " << message << std::endl;
+    }
+    
+    void Warn(const char* message)
+    {
+        if (m_LogLevel >= LogLevelWarning)
+            std::cout << "[WARNING]: " << message << std::endl;
+    }
+    
+    void Info(const char* message)
+    {
+        if (m_LogLevel >= LogLevelInfo)
+            std::cout << "[INFO]: " << message << std::endl;
+    }
+};
+
+int main()
+{
+    Log log;
+    log.SetLevel(log.LogLevelError);
+    
+    log.Warn("Hello");
+    log.Info("Hello");
+    log.Error("Hello");
+
+    std::cin.get();
+}

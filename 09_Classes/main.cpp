a21be108077f19dc11d6965ccb3c829a259e17f6
Josh Newham <joshnewham@live.com>
2023-05-21 15:44:43 +0100

    First lesson on classes

---
+++ 09_Classes/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+#define LOG(x) std::cout << x << std::endl
+
+// Classes help group data and methods together. Anything you can do with a class, you can do without a class.
+// They just help make code easier to maintain
+class Player
+{
+public:
+    int x, y;
+    int speed;
+    
+    void Move(int xa, int ya)
+    {
+        // Modifies the current player's x and y
+        x += xa * speed;
+        y += ya * speed;
+    }
+};
+
+int main()
+{
+    Player player;
+    player.x = 5;
+    player.Move(1, -1);
+
+    std::cin.get();
+}
